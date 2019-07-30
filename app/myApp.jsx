86daa501dd3574353aba7253277650701bb5315f
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-30 22:52:45 +0300

    Create myApp.jsx

---
+++ app/myApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import Components:
+import ReactPosts from './components/ReactPosts';
+
+ReactDOM.render(<ReactPosts />, document.getElementById('root'));
